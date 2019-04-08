1fc0b95655a3ca9833357dcaf42d6acfaa0a909b
sonphan18071999 <43442285+sonphan18071999@users.noreply.github.com>
2019-04-08 13:11:37 +0700

    mmm

--- QuanLiXe/QuanLiXe/MY_DB.cs
+++ QuanLiXe/QuanLiXe/MY_DB.cs
@@ -10,7 +10,8 @@ namespace QuanLiXe
 {
     class MY_DB
     {
-        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\LTWindows\QuanLiXe\QuanLiXe\DESKTOP-RRRHOP4.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Do_An_Win\LapTrinhWin\QLXe\QuanLiXe\QuanLiXe\DESKTOP-RRRHOP4.mdf;Integrated Security=True;Connect Timeout=30");
+        //SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\LTWindows\QuanLiXe\QuanLiXe\DESKTOP-RRRHOP4.mdf;Integrated Security=True;Connect Timeout=30");
         public SqlConnection getConnection
         {
             get { return conn; }
